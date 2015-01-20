1882347f78c6d46073c1f7769bcecfede04520c2
Shabai Liu <sliu@cvent.com>
2015-01-20 15:11:17 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to Run
+
+1. `npm install`
+2. `npm start`
+3. Open `index.html`
---
+++ js/app.js
@@ -0,0 +1,10 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react');
+
+React.renderComponent(
+  <h1>Hello World</h1>,
+  document.getElementById('react')
+);
